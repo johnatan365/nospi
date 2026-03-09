b8266d6de3de467bd8d45d70d567ea28be10e46c
natively.dev <api@natively.dev>
2026-03-09 09:39:03 +0000

    iconos iphone

--- app/(tabs)/_layout.ios.tsx
+++ app/(tabs)/_layout.ios.tsx
@@ -8,7 +8,7 @@ export default function TabLayout() {
     {
       name: 'events',
       route: '/(tabs)/events',
-      icon: 'today',
+      icon: 'local-activity',
       label: 'Eventos',
     },
     {
